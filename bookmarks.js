ca5c9136f7b7dad0e73b027c6a566784d630acf1
Artem Kaloshin <a.i.kaloshin@gmail.com>
2023-08-25 23:06:41 +0300

    Add sms notification

--- bookmarks.js
+++ bookmarks.js
@@ -15,7 +15,6 @@ const bookmarks = [
       { name: "Vault", url: "https://v2u-vault.fly.dev" },
       { name: "Intranet Mysql", url: "http://mysql.mw" },
       { name: "Mail alias", url: "http://mailalias.mw" },
-      { name: "S3 storage", url: "http://s3storage.mw:9001" },
     ],
   },
   {
@@ -30,6 +29,14 @@ const bookmarks = [
     links: [
       { name: "SymfonyCasts", url: "https://symfonycasts.com/" },
       { name: "Laravel news", url: "https://laravel-news.com/category/news"}
+    ]
+  },
+  {
+    title: "Storage",
+    links: [
+      { name: "Minio storage", url: "http://s3storage.mw:9001" },
+      { name: "Backblaze", url: "https://secure.backblaze.com/b2_buckets.htm"},
+      { name: "AWS", url: "https://s3.console.aws.amazon.com/s3/home?region=eu-central-1"}
     ]
   }
 ];
