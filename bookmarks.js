0aa4b6a812eb7eab5c7a7a42e7d00899d67dea48
Artem Kaloshin <a.i.kaloshin@gmail.com>
2023-09-29 12:11:42 +0300

    Change panel order

--- bookmarks.js
+++ bookmarks.js
@@ -10,9 +10,16 @@ const bookmarks = [
     ],
   },
   {
+    title: "Storage",
+    links: [
+      { name: "Minio storage", url: "http://s3storage.mw:9001" },
+      { name: "Backblaze", url: "https://secure.backblaze.com/b2_buckets.htm"},
+      { name: "AWS", url: "https://s3.console.aws.amazon.com/s3/home?region=eu-central-1"}
+    ]
+  },
+  {
     title: "Tools",
     links: [
-      { name: "Vault", url: "https://v2u-vault.fly.dev" },
       { name: "Intranet Mysql", url: "http://mysql.mw" },
       { name: "Mail alias", url: "https://mailalias.localhost.direct" },
     ],
@@ -32,19 +39,15 @@ const bookmarks = [
     ]
   },
   {
-    title: "Storage",
-    links: [
-      { name: "Minio storage", url: "http://s3storage.mw:9001" },
-      { name: "Backblaze", url: "https://secure.backblaze.com/b2_buckets.htm"},
-      { name: "AWS", url: "https://s3.console.aws.amazon.com/s3/home?region=eu-central-1"}
-    ]
-  },
-  {
     title: "Hosting",
     links: [
       {
         name: "Fly.io",
         url: "https://fly.io/dashboard"
+      },
+      {
+        name: "DomCLoud",
+        url: "https://my.domcloud.co/login"
       }
     ]
   }
