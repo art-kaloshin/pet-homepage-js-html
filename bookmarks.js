57230ac0893ef34a39caa119ae489bfb508d3ebc
ArtemKaloshin <t@v2u.su>
2024-02-06 23:07:48 +0300

    Add home assistant

--- bookmarks.js
+++ bookmarks.js
@@ -1,13 +1,21 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Personal",
+    title: "VPN Services",
     links: [
       { name: "Todo", url: "https://todo.localhost.direct" },
-      { name: "Wiki", url: "http://wiki.mw" },
       { name: "WP notes", url: "http://notes.mw" },
+      { name: "HOME Assistant", url: "http://10.8.0.5:8123/"},
       { name: "Transmission", url: "http://10.8.0.5:9091/transmission/web/"},
       { name: "Syncthing", url: "http://10.8.0.5:8384/"},
+    ],
+  },
+  {
+    title: "LAN Services",
+    links: [
+      { name: "HOME Assistant", url: "http://192.168.1.5:8123/"},
+      { name: "Transmission", url: "http://192.168.1.5:9091/transmission/web/"},
+      { name: "Syncthing", url: "http://192.168.1.5:8384/"},
     ],
   },
   {
@@ -30,13 +38,6 @@ const bookmarks = [
     links: [
       { name: "Yota", url: "https://lk.yota.ru"},
       { name: "Netis", url: "https://stat.netis.ru/"}
-    ]
-  },
-  {
-    title: "Symfony & Laravel",
-    links: [
-      { name: "SymfonyCasts", url: "https://symfonycasts.com/" },
-      { name: "Laravel news", url: "https://laravel-news.com/category/news"}
     ]
   },
   {
